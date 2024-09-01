7fe72321b6b025f135c1ce60c9a84249237e2514
Deva Midhun <dev@deva.is-cool.dev>
2024-09-01 10:11:22 +0530

    update

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify, render_template
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
+import hashlib
 
 app = Flask(__name__)
 limiter = Limiter(
@@ -10,15 +11,57 @@ limiter = Limiter(
     storage_uri="memory://",
 )
 
+def md5(fname):
+    with open(fname, "rb") as file:
+        return hashlib.md5(file.read()).hexdigest()  # Changed to read the entire file
+
+def increment_version(version):
+    major, minor, patch = map(int, version.split('.'))
+    patch += 1
+    return f"{major}.{minor}.{patch}"
+
+current_hash = md5(__file__)
+
+try:
+    with open('data/md5.txt', 'r') as f:
+        previous_hash = f.read().strip()
+except FileNotFoundError:
+    previous_hash = ''
+
+if current_hash != previous_hash:
+    try:
+        with open('data/ver.txt', 'r') as f:
+            oldver = f.read().strip()
+    except FileNotFoundError:
+        oldver = '0.0.0'
+    
+    new_version = increment_version(oldver)
+    
+    with open('data/ver.txt', 'w') as f:
+        f.write(new_version)
+    
+    with open('data/md5.txt', 'w') as f:
+        f.write(current_hash)
+else:
+    with open('data/ver.txt', 'r') as f:
+        new_version = f.read().strip()
+
+
 @app.route('/')
 @limiter.exempt
 def home():
-    return render_template('index.html')
+    return render_template('index.html', version=new_version)
 
 @app.route('/tlds', methods=['GET'])
 @limiter.exempt
 def get_tlds():
-    return open('tlds.json').read()
+    return open('data/tlds.json').read()
+
+@app.route('/domains')
+@limiter.exempt
+def get_domains():
+    return open('data/domains.json').read()
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
